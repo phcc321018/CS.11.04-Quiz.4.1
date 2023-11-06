65f99f5cce24a407d7930d7f600f86325820f77e
phcc321018 <phcc32@student.cis.edu.hk>
2023-11-06 12:16:47 +0800

    lab 4.1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+
+    public static int numberOfVowels(String str) {
+        char[] strArray = str.toCharArray();
+        int pat = 0;
+        for (char c : strArray) {
+            if (((c == 'a' || c == 'e') || (c == 'i' || c == 'o')) || c == 'u') pat++;
+        }
+        return pat;
+    }
+
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -15,6 +25,18 @@ public class Main {
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
+    public class NumberChecker {
+        public static int notDivisibleBy235(int N) {
+            int count = 0;
+
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -25,5 +47,25 @@ public class Main {
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
 
+    public class CamelCaseConverter {
+        public static String camelCaseMe(String input) {
+            StringBuilder result = new StringBuilder();
 
-}
+            boolean capitalizeNext = false;
+            for (int i = 0; i < input.length(); i++) {
+                char currentChar = input.charAt(i);
+                if (currentChar == '-' || currentChar == '_') {
+                    capitalizeNext = true;
+                } else {
+                    if (capitalizeNext) {
+                        result.append(Character.toUpperCase(currentChar));
+                        capitalizeNext = false;
+                    } else {
+                        result.append(Character.toLowerCase(currentChar));
+                    }
+                }
+            }
+
+            return result.toString();
+        }
+    }}}
